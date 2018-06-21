8fc327c7bde94ca6653d250091bbd333dbadb91c
otherside9710 <jsarmientop.inca@gmail.com>
2018-06-20 21:39:07 -0500

    new maquetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NofoundPageComponent } from './components/nofound-page/nofound-page.component';
+import { SuccessPageComponent} from './components/success-page/success-page.component';
+import { NavComponent } from './components/nav/nav.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AppComponent} from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'sing-up', component: RegisterComponent},
+  {path: 'success-Page', component: SuccessPageComponent},
+  {path: '**', component: NofoundPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
